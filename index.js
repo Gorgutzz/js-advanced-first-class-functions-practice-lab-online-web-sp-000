fa6250f754833a8891da34f3af20ad8fd4d90398
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-12 01:49:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,7 @@ const logDriversByHometown = function(drivers, location) {
 };
 
 const driversByRevenue = function(drivers){
-  return drivers.slice().sort(function(a, b) => (a.revenue - b.revenue));
+  return drivers.slice().sort(function(a, b) {
+    return a.revenue - b.revenue
+  })
   }
